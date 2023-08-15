d654c7e8836753fa97170974755eb25e412c2dd8
Garin Yaroslav <garinyaroslav13@mail.ru>
2023-08-15 21:48:33 +0300

    first commit

---
+++ src/components/PizzaBlock/Skeleton.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ContentLoader from 'react-content-loader';
+
+const Skeleton = () => (
+  <ContentLoader
+    className="pizza-block"
+    speed={2}
+    width={280}
+    height={465}
+    viewBox="0 0 280 465"
+    backgroundColor="#f3f3f3"
+    foregroundColor="#ecebeb">
+    <circle cx="135" cy="128" r="120" />
+    <rect x="0" y="300" rx="10" ry="10" width="280" height="88" />
+    <rect x="-1" y="417" rx="10" ry="10" width="90" height="45" />
+    <rect x="127" y="417" rx="25" ry="25" width="152" height="45" />
+    <rect x="1" y="259" rx="10" ry="10" width="280" height="29" />
+  </ContentLoader>
+);
+
+export default Skeleton;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import Categories from '../components/Categories';
+import Sort from '../components/Sort';
+import PizzaBlock from '../components/PizzaBlock';
+import Skeleton from '../components/PizzaBlock/Skeleton';
+
+export default function Home() {
+  const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+  React.useEffect(() => {
+    (async () => {
+      try {
+        await fetch('http://localhost:3001/items')
+          .then((res) => res.json())
+          .then((json) => {
+            setItems(json);
+            setIsLoading(false);
+          });
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+    window.scrollTo(0, 0);
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="content__top">
+        <Categories />
+        <Sort />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {isLoading
+          ? [...new Array(10)].map((_, index) => <Skeleton key={index} />)
+          : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
+      </div>
+    </div>
+  );
+}
