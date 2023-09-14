2823b38a32e566b5081bb163221cb6a9dfcb2856
Garin Yaroslav <garinyaroslav13@mail.ru>
2023-09-14 10:20:48 +0300

    hide mokapi address

--- src/redux/pizza/asyncActions.ts
+++ src/redux/pizza/asyncActions.ts
@@ -1,14 +1,14 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { SearchPizzaParams, Pizza } from './types';
 import axios from 'axios';
+
+import { getPizzaByAddress } from '../../utils/getPizzasByAddress';
+import { SearchPizzaParams, Pizza } from './types';
 
 export const fetchPizzas = createAsyncThunk(
   'pizza/fetchPizzasStatus',
   async (params: SearchPizzaParams) => {
-    const { category, sortBy, order, search, currentPage } = params;
-    const { data } = await axios.get<Pizza[]>(
-      `http://localhost:3001/items?${category}${search}&_sort=${sortBy}&_order=${order}&_limit=4&_page=${currentPage}`,
-    );
+    const getPizza = getPizzaByAddress(params);
+    const { data } = await axios.get<Pizza[]>(getPizza);
     return data;
   },
 );
